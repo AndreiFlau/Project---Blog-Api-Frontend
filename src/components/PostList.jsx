05c6a29f35b5f41d869b9f9bcf40d2f27698d21c
Andrei <andreiflausino@live.com>
2024-10-30 21:15:24 -0300

    Added a spinning wheel for when loading posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,32 +3,56 @@ import "../styles/App.css";
 import useAuth from "../hooks/useAuth";
 import formatDate from "../formatDate";
 import usePosts from "../hooks/usePosts";
+import { useState } from "react";
 
 function PostList() {
   const { userData } = useAuth();
   const { posts, loading, error } = usePosts();
+  const [loadingWheel, setLoadingWheel] = useState(false);
+  const sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+  function loadingWheelFunc() {
+    setLoadingWheel(!loadingWheel);
+  }
 
   return (
     <>
       {userData ? (
         loading ? (
-          <h1>Loading...</h1>
+          <div className="loading">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+              <circle
+                className="spinner"
+                cx="25"
+                cy="25"
+                r="20"
+                fill="none"
+                stroke="#4F46E5"
+                strokeWidth="4"
+                strokeLinecap="round"
+                strokeDasharray="80"
+                strokeDashoffset="60"
+              />
+            </svg>
+            <h1>Loading blog posts...</h1>
+          </div>
         ) : error ? (
           <div>
             <h1>Oops... something happened: {error.message}</h1>
           </div>
-        ) : (
+        ) : !loadingWheel ? (
           <div className="posts">
             <h1>Posts</h1>
-
             <ul>
-              {posts.map(
+              {sortedPosts.map(
                 (post) =>
                   post.published && (
                     <li key={post.id}>
                       <div>
                         <h2>
-                          <Link to={`/post/${post.id}`}>{post.title}</Link>
+                          <Link to={`/post/${post.id}`} onClick={loadingWheelFunc}>
+                            {post.title}
+                          </Link>
                         </h2>
                         <p>{formatDate(post.date)}</p>
                         <p>By: {post.author}</p>
@@ -37,6 +61,24 @@ function PostList() {
                   )
               )}
             </ul>
+          </div>
+        ) : (
+          <div className="loading">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+              <circle
+                className="spinner"
+                cx="25"
+                cy="25"
+                r="20"
+                fill="none"
+                stroke="#4F46E5"
+                strokeWidth="4"
+                strokeLinecap="round"
+                strokeDasharray="80"
+                strokeDashoffset="60"
+              />
+            </svg>
+            <h1>Loading blog post...</h1>
           </div>
         )
       ) : (
